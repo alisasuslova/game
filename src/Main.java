f1e91b5011d9c65f686b74ebb038ca13e291abc0
alisasuslova <alisasuslova1408@gmail.com>
2022-01-19 19:42:14 +0300

    19.01.22 changes Main_1

--- src/Main.java
+++ src/Main.java
@@ -7,137 +7,38 @@ import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
-        LocalDateTime localDateTime = LocalDateTime.now();
-        StringBuilder sb = new StringBuilder();
+        List<String> dirName = new LinkedList<>();
+        dirName.add("res");
+        dirName.add("savegames");
+        dirName.add("temp");
+        dirName.add("src/main");
+        dirName.add("src/test");
+        dirName.add("res/drawables");
+        dirName.add("res/vectors");
+        dirName.add("res/icons");
 
+        writeToFile(makeDir(dirName));
 
-        File dir = new File("res");
-        if (dir.mkdir())
-            sb.append(localDateTime)
-                    .append(" ")
-                    .append("Каталог res создан")
-                    .append("\n");
+        List<String> fileName = new LinkedList<>();
+        fileName.add("src/main/Main.java1");
+        fileName.add("src/main/Utils.java1");
+        fileName.add("temp/temp.txt");
 
-
-        File dir1 = new File("savegames");
-        if (dir1.mkdir())
-            sb.append(localDateTime)
-                    .append(" ")
-                    .append("Каталог savegames создан")
-                    .append("\n");
-
-
-        File dir2 = new File("temp");
-        if (dir2.mkdir())
-            sb.append(localDateTime)
-                    .append(" ")
-                    .append("Каталог temp создан")
-                    .append("\n");
-
-
-        File dir3 = new File("src/main");
-        if (dir3.mkdir())
-            sb.append(localDateTime)
-                    .append(" ")
-                    .append("В каталоге src создан новый каталог main")
-                    .append("\n");
-
-
-        File dir4 = new File("src/test");
-        if (dir4.mkdir())
-            sb.append(localDateTime)
-                    .append(" ")
-                    .append("В каталоге src создан новый каталог test")
-                    .append("\n");
-
-
-        File file1 = new File("src/main/Main.java");
-        try {
-            if (file1.createNewFile())
-                sb.append(localDateTime)
-                        .append(" ")
-                        .append("Файл Main.java был создан в каталоге main")
-                        .append("\n");
-
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        File file2 = new File("src/main/Utils.java");
-        try {
-            if (file2.createNewFile())
-                sb.append(localDateTime)
-                        .append(" ")
-                        .append("Файл Utils.java был создан в каталоге main")
-                        .append("\n");
-
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        File dir5 = new File("res/drawables");
-        if (dir5.mkdir())
-            sb.append(localDateTime)
-                    .append(" ")
-                    .append("В каталоге res создана новая директория drawables")
-                    .append("\n");
-
-
-        File dir6 = new File("res/vectors");
-        if (dir6.mkdir())
-            sb.append(localDateTime)
-                    .append(" ")
-                    .append("В каталоге res создана новая директория vectors")
-                    .append("\n");
-
-
-        File dir7 = new File("res/icons");
-        if (dir7.mkdir())
-            sb.append(localDateTime)
-                    .append(" ")
-                    .append("В каталоге res создана новая директория icons")
-                    .append("\n");
-
-
-        File file3 = new File("temp/temp.txt");
-        try {
-            if (file3.createNewFile())
-                sb.append(localDateTime)
-                        .append(" ")
-                        .append("Файл temp.txt был создан в каталоге temp")
-                        .append("\n");
-
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        String result = sb.toString();
-
-        try (FileOutputStream fos = new FileOutputStream("temp/temp.txt", true)) {
-            byte[] bytes = result.getBytes();
-            fos.write(bytes, 0, bytes.length);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+        writeToFile(makeFile(fileName));
 
         GameProgress gameProgress1 = new GameProgress(70, 2, 3, 3.6);
         String path1 = "savegames/gameProgress1.dat";
-        saveGame(path1, gameProgress1);
-
 
         GameProgress gameProgress2 = new GameProgress(85, 4, 5, 5.8);
         String path2 = "savegames/gameProgress2.dat";
-        saveGame(path2, gameProgress2);
 
         GameProgress gameProgress3 = new GameProgress(90, 5, 9, 7.6);
         String path3 = "savegames/gameProgress3.dat";
-        saveGame(path3, gameProgress3);
 
         GameProgress gameProgress4 = new GameProgress(120, 10, 21, 21.3);
         String path4 = "savegames/gameProgress4.dat";
-        saveGame(path4, gameProgress4);
 
         List<String> pathes = new LinkedList<>();
         pathes.add(path1);
@@ -145,20 +46,78 @@ public class Main {
         pathes.add(path3);
         pathes.add(path4);
 
+        List<GameProgress> gameProgress = new LinkedList<>();
+        gameProgress.add(gameProgress1);
+        gameProgress.add(gameProgress2);
+        gameProgress.add(gameProgress3);
+        gameProgress.add(gameProgress4);
+
+        saveGame(pathes,  gameProgress);
+
         String zipPath = "savegames/gameSaves.zip";
         zipFiles(zipPath, pathes);
 
-
         deleteFiles(pathes);
         String openZipPath = "savegames/";
+
         openZip(zipPath, openZipPath);
 
+        List<String> packed_pathes = new LinkedList<>();
+        packed_pathes.add("savegames/packed_gameProgress1.dat");
+        packed_pathes.add("savegames/packed_gameProgress2.dat");
+        packed_pathes.add("savegames/packed_gameProgress3.dat");
+        packed_pathes.add("savegames/packed_gameProgress4.dat");
 
-        System.out.println(openProgress("savegames/packed_gameProgress1.dat"));
-        System.out.println(openProgress("savegames/packed_gameProgress2.dat"));
-        System.out.println(openProgress("savegames/packed_gameProgress3.dat"));
-        System.out.println(openProgress("savegames/packed_gameProgress4.dat"));
+        System.out.println(openProgress(packed_pathes));
 
+    }
+
+    public static void writeToFile(String  result) {
+
+        try (FileOutputStream fos = new FileOutputStream("temp/temp.txt", true)) {
+            byte[] bytes = result.getBytes();
+            fos.write(bytes, 0, bytes.length);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public  static  String  makeDir(List<String> dirName) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < dirName.size(); i++) {
+            File dir = new File(dirName.get(i));
+            if (dir.mkdir())
+                sb.append(localDateTime)
+                        .append(" ")
+                        .append("Каталог " + dirName.get(i) + " создан")
+                        .append("\n");
+        }
+        String result = sb.toString();
+        return result;
+    }
+
+    public  static  String makeFile(List<String> fileName) throws IOException {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < fileName.size(); i++) {
+            File file = new File(fileName.get(i));
+            if (file.createNewFile())
+                sb.append(localDateTime)
+                        .append(" ")
+                        .append("Файл " + fileName.get(i) + " был создан")
+                        .append("\n");
+
+            if(fileName.get(i).equals("temp/temp.txt"))
+                sb.append(localDateTime)
+                        .append(" ")
+                        .append("Файл " + fileName.get(i) + " был создан")
+                        .append("\n");
+        }
+        String result = sb.toString();
+        return result;
     }
 
     public static void zipFiles(String zipPath, List<String> pathes) throws FileNotFoundException {
@@ -203,33 +162,46 @@ public class Main {
         }
     }
 
-    public static GameProgress openProgress(String packed_pathes) {
-
+    public static List<GameProgress> openProgress(List<String> packed_pathes) {
+        List<GameProgress>  www = new LinkedList<>();
         GameProgress rezult = null;
-        try (FileInputStream fis = new FileInputStream(packed_pathes);
-             ObjectInputStream ois = new ObjectInputStream(fis)) {
-            rezult = (GameProgress) ois.readObject();
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+        for (int i = 0; i < packed_pathes.size(); i++) {
+
+            try (FileInputStream fis = new FileInputStream(packed_pathes.get(i));
+                 ObjectInputStream ois = new ObjectInputStream(fis)) {
+                rezult = (GameProgress) ois.readObject();
+            }
+            catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+            www.add(rezult);
         }
-        return rezult;
+        return www;
     }
 
+    public static void saveGame(List<String> pathes, List<GameProgress> gameProgress) {
 
-    public static void saveGame(String path, GameProgress gameProgress) {
-
-        File file = new File(path);
-        try {
-            if (file.createNewFile())
-                System.out.println("Сохранение прогресса произведено. Файл " + path + " записан.");
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        try (FileOutputStream fos = new FileOutputStream(path);
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            oos.writeObject(gameProgress);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+        for (int i = 0; i < pathes.size(); i++) {
+            for (int j = 0; j < gameProgress.size(); j++) {
+                File file = new File(pathes.get(i));
+                try {
+                    if (file.createNewFile())
+                        System.out.println("Сохранение прогресса произведено. Файл " + pathes.get(i) + " записан.");
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+                if(i == j) {
+                    try (FileOutputStream fos = new FileOutputStream(pathes.get(i));
+                         ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+                        oos.writeObject(gameProgress.get(j));
+                    } catch (Exception ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                }
+                else {
+                    continue;
+                }
+            }
         }
     }
 
@@ -237,11 +209,9 @@ public class Main {
 
         for (int i = 0; i < pathes.size(); i++) {
             File file = new File(pathes.get(i));
-
             if (file.delete())
                 System.out.println("Файл " + pathes.get(i) + " удален");
         }
     }
-
 }
 
